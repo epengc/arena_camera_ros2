d93eeeb5579179188f2cfb96f5890eed3e7ccffa
epengc <epengc@icloud.com>
2024-05-02 01:45:49 -0700

    update for disparity_node.cpp

--- ros2_ws/stereo_image_proc/src/disparityMap.cpp
+++ ros2_ws/stereo_image_proc/src/disparityMap.cpp
@@ -25,7 +25,7 @@
 #include "message_filters/syn_policies/exact_time.h"
 #include "message_filters/synchronizer.h"
 
-using ExactPolicy = message_filters::sync_policies::ExactTime<sensor_msgs::msg::Image,
+using ExactPolicy = message_filters::sync_policies::ExactTime<sensor_msgs::msg::images,
                                                               sensor_msgs::msg::CameraInfo,
                                                               sensor_msgs::msg::Image,
                                                               sensor_msgs::msg::CameraInfo>;
@@ -33,16 +33,18 @@ using ApproximatePolicy = message_filters::sync_policies::ApproximateTime<sensor
                                                                           sensor_msgs::msg::CameraInfo,
                                                                           sensor_msgs::msg::Image,
                                                                           sensor_msgs::msg::CameraInfo>;
-using ApproximateEpsilonPolicy = message_filters::sync_policies::ApproximateEpsilonPolicy<sensor_msgs::msg::Image,
-                                                                                          sensor_msgs::msg::CameraInfo,
-                                                                                          sensor_msgs::msg::Image,
-                                                                                          sensor_msgs::msg::CameraInfo>;
+using ApproximateEpsilonPolicy = message_filters::sync_policies::ApproximateEpsilonTime<sensor_msgs::msg::Image,
+                                                                                        sensor_msgs::msg::CameraInfo,
+                                                                                        sensor_msgs::msg::Image,
+                                                                                        sensor_msgs::msg::CameraInfo>;
 using ExactSync = message_filters::Synchronizer<ExactPolicy>;
 using ApproximateSync = message_filters::Synchronizer<ApproximatePolicy>;
-using ApproximateEpsilonPolicy = message_filters::Synchronizer<ApproximateEpsilonPolicy>;
+using ApproximateEpsilonSync = message_filters::Synchronizer<ApproximateEpsilonPolicy>;
 
 namespace stereo_image_proc
 {
+
+
 class DisparityNode : public rclcpp::Node
 {
  public:
@@ -75,6 +77,7 @@ class DisparityNode : public rclcpp::Node
   rcl_interfaces::msg::SetParametersResult parameterSetCb(const std::vector<rclcpp::Parameter> & parameters);
 };
 
+
   // Some helper functions for adding a parameter to a collection
   static void add_param_to_map(std::map<std::string, std::pair<int, rcl_interfaces::msg::ParameterDescriptor>> & parameters,
                                const std::string & name,
@@ -95,5 +98,69 @@ class DisparityNode : public rclcpp::Node
   }
 
 
+  static void add_param_to_map(std::map<std::string, std::pair<double, rcl_interfaces::msg::ParameterDescriptor>> & parameters,
+                               const std::string& name,
+                               const std::string& description,
+                               const double default_value,
+                               const double from_value,
+                               const double to_value,
+                               const double step)
+  {
+    rcl_interfaces::msg::FloatingPointRange floating_point_range;
+    floating_point_range.from_value = from_value;
+    floating_point_range.to_value = to_value;
+    floating_point_range.step = step;
+    rcl_interfaces::msg::ParameterDescriptor descriptor;
+    descriptor.description = description;
+    descriptor.floating_point_range = {floating_point_range};
+    parameter[name] = std::make_pair(default_value, descriptor);
+  }
+
+
+  DisparityNode::DisparityNode(const rclcpp::NodeOptions & options):rclcpp::Node("diparity_node", options)
+  {
+    using namespace std::placeholders;
+    // TransportHints does not actually declare the parameter
+    this->declare_parameter<std::string>("image_transport", "raw");
+    // Declare/read parameters
+    int queue_size = this->declare_parameters("queue_size", 5);
+    bool approx = this->declare_parameters("approximate_sync", false);
+    double approx_sync_epsilon = this->declare_parameter("approximate_sync_tolerance_seconds", 0.0);
+    this->declare_parameter("use_system_default_qos", false);
+
+    // Synchronize callbacks
+    if(approx){
+      if(0.0==approx_sync_epsilon){
+        approximate_sync_.reset(new ApproximateSync(ApproximateEpsilonPolicy(queue_size)),
+                                sub_l_image_,
+                                sub_l_info_,
+                                sub_r_image_,
+                                sub_r_info_);
+        approximate_sync_->registerCallback(std::bind(&DisparityNode::imageCb, this, _1, _2, _3, _4));
+      }else{
+        approximate_epsilon_sync_.reset(new ApporximateEpsilonSync(ApproximateEpsilonPolicy(queue_size, rclcpp::Duration::from_seconds(approx_sync_epsilon)),
+                                                                   sub_l_image_,
+                                                                   sub_l_info_,
+                                                                   sub_r_image_,
+                                                                   sub_r_info_));
+        approximate_epsilon_sync_->registerCallback(std::bind(&DisparityNode::imageCb, this, _1, _2, _3, _4));
+      }
+    }else{
+      exact_sync_.reset(new ExactSync(ExactPolicy(queue_size),
+                                      sub_l_image,
+                                      sub_l_info,
+                                      sub_r_image,
+                                      sub_r_info));
+      exact_sync_->registerCallback(std::bind(&DisparityNode::imageCb, this, _1, _2, _3, _4));
+    }
+
+    // Register a callback for when parameters are set
+    on_set_parameters_callback_handle_ = this->add_on_set_parameters_callback(std::bind(&DisparityNode::parameterSetCb,
+                                                                                        this,
+                                                                                        _1));
+    // Describe int paramters
+    std::map<std::string, std::pair<int, rcl_interface::msg::ParameterDescriptor>> int_params;
+
+  }
 
 }  // namespace stereo_image_proc
